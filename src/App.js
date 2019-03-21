5a3d0c5558ab0257886c6aac8e3d9e28bc6ab6f2
Elizabeth King <elizking.dev@gmail.com>
2019-03-20 19:58:36 -0600

    added contacts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header } from 'semantic-ui-react';
+import Contacts from './contacts/Contacts';
+
 
 class App extends Component {
+  state = {
+    contacts: [
+      {id: 1, firstName: 'Bob', phone: '123-123-1234'},
+      {id: 1, firstName: 'Rob', phone: '123-223-1234'},
+      {id: 1, firstName: 'Kob', phone: '123-323-1234'}
+    ]
+  }
+
   render() {
+    const { contacts } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Header as='h1'>React Contact List</Header>
+        <Contacts contacts={contacts}/>
+      </Container>
     );
   }
 }
